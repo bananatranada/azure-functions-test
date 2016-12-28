5f523e043e2589542aedfc3ba8d94aed4ec38d4c
bkgunby <bkgunby@users.noreply.github.com>
2016-12-28 05:59:42 -0600

    init

---
+++ bye/index.js
@@ -0,0 +1,23 @@
+module.exports = function (context, req) {
+
+    // var category = context.bindingData.category;
+    // var id = context.bindingData.id;
+
+    // if (!id) {
+    //     context.res = {
+    //         // status: 200, /* Defaults to 200 */
+    //         body: "All " + category + " items were requested."
+    //     };
+    // }
+    // else {
+    //     context.res = {
+    //         // status: 200, /* Defaults to 200 */
+    //         body: category + " item with id = " + id + " was requested."
+    //     };
+    // }
+    context.res = {
+      body: 'bye'
+    }
+
+    context.done();
+}
